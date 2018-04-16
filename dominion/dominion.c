25c208a91897be2436acdce840c8bdb38fc9148b
Jaegermeiste <jason.grant.george@gmail.com>
2018-04-15 20:30:45 -0700

    Revert "Refactor cardEffect Adventurer and Smithy"
    This reverts commit 47d35c10dd87ba77c741a26eef0a08919a83b188.

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -643,53 +643,6 @@ int getCost(int cardNumber)
   return -1;
 }
 
-/*
- * BEGIN ASSIGNMENT 2 REFACTOR
- */
-int cardEffectAdventurer (int currentPlayer, struct gameState *state)
-{
-	int drawnTreasure = 0;
-	int cardDrawn = 0;
-	int tempHand[MAX_HAND];// moved above the if statement
-	int tempHandCounter = 0;// this is the counter for the temp hand
-
-	while (drawnTreasure < 2) {
-		if (state->deckCount[currentPlayer] < 1) {//if the deck is empty we need to shuffle discard and add to deck
-			shuffle(currentPlayer, state);
-		}
-		drawCard(currentPlayer, state);
-		cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer] - 1];//top card of hand is most recently drawn card.
-		if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-			drawnTreasure++;
-		else {
-			tempHand[tempHandCounter] = cardDrawn;
-			state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-			tempHandCounter++;
-		}
-	}
-	while (tempHandCounter - 1 >= 0) {
-		state->discard[currentPlayer][state->discardCount[currentPlayer]++] = tempHand[tempHandCounter - 1]; // discard all cards in play that have been drawn
-		tempHandCounter = tempHandCounter - 1;
-	}
-	return 0;
-}
-
-int cardEffectSmithy(int currentPlayer, struct gameState *state, int handPos)
-{
-	//+3 Cards
-	for (unsigned i = 0; i < 3; i++)
-	{
-		drawCard(currentPlayer, state);
-	}
-
-	//discard card from hand
-	discardCard(handPos, currentPlayer, state, 0);
-	return 0;
-}
-/*
-* END ASSIGNMENT 2 REFACTOR
-*/
-
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -702,7 +655,8 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-
+  int drawntreasure=0;
+  int cardDrawn;
   int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
@@ -713,8 +667,26 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-		return cardEffectAdventurer(currentPlayer, state);				// JG refactor
-					
+      while(drawntreasure<2){
+	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+	  shuffle(currentPlayer, state);
+	}
+	drawCard(currentPlayer, state);
+	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+	  drawntreasure++;
+	else{
+	  temphand[z]=cardDrawn;
+	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+	  z++;
+	}
+      }
+      while(z-1>=0){
+	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+	z=z-1;
+      }
+      return 0;
+			
     case council_room:
       //+4 Cards
       for (i = 0; i < 4; i++)
@@ -857,7 +829,15 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-		return cardEffectSmithy(currentPlayer, state, handPos);	// JG refactor
+      //+3 Cards
+      for (i = 0; i < 3; i++)
+	{
+	  drawCard(currentPlayer, state);
+	}
+			
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;
 		
     case village:
       //+1 Card
